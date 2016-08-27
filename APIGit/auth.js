0f39845d427e551d2355315f06bd5c319bde297a
ftuyama <ftuyama@gmail.com>
2016-08-27 17:54:43 -0300

    Login Git - fixed

--- APIGit/auth.js
+++ APIGit/auth.js
@@ -13,23 +13,34 @@ var client;
   ===========================================================================
 */
 
-// Loading login information stored in json
-fs.readFile('./APIGit/client_secret.json', function processClientSecrets(err, content) {
-    if (err) {
-        console.log('Error loading client secret file: ' + err);
-        return;
-    }
-    // Load the credentials
-    credentials = JSON.parse(content);
-    client = github.client(credentials.web.client_id);
-
-    console.log("User logged in!");
+/* GET Git login */
+router.get('/auth', function(req, res) {
+    // Loading login information stored in json
+    fs.readFile('./APIGit/client_secret.json', function processClientSecrets(err, content) {
+        if (err) {
+            console.log('Error loading client secret file: ' + err);
+            return;
+        }
+        // Load the credentials
+        console.log("User logged in!");
+        credentials = JSON.parse(content);
+        req.session.git_access_token = credentials.web.access_token;
+        res.cookie('git_token', credentials.web.access_token);
+        res.redirect('/project/');
+    });
 });
 
 /* GET Git list */
 router.get('/', function(req, res) {
-    client = github.client("45cfd61f7c8c3729435cbec5b38fa9d51dd4968c");
+    if (!req.session.git_access_token && !req.cookies.git_token) return res.redirect('/project/auth');
+    if (!req.session.git_access_token) req.session.git_access_token = req.cookies.git_token;
+
+    //Create an instance from accessToken
+    var accessToken = req.session.git_access_token;
+    console.log(accessToken);
+    client = github.client(accessToken);
     client.get('/user', {}, function(err, status, body, headers) {
+        if (err) return res.status(500).send(err);
         var page = fs.readFileSync(__dirname + "/../web/view/principal.html", "utf8");
         res.send(mustache.to_html(page, { username: body.login }));
     });

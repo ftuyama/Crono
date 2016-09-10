1cee3ec071c6ee82155458b5b4b7520b24eda6ad
joaovitormarques <joaovitormarques17@gmail.com>
2016-09-10 16:14:45 -0300

    hello world firebase DB

--- web/server/principalSV.js
+++ web/server/principalSV.js
@@ -4,6 +4,13 @@ var router = express.Router();
 var fs = require("fs");
 var bodyParser = require('body-parser');
 var mustache = require('mustache');
+var firebase = require("firebase");
+var myFirebaseRef;
+
+firebase.initializeApp({
+    databaseURL: 'https://friendlychat-e3ac2.firebaseio.com/',
+    serviceAccount: 'FriendlyChat-dd9d136c0ce2.json', //this is file that I downloaded from Firebase Console
+});
 
 /* GET home page */
 router.get('/', function(req, res) {
@@ -22,6 +29,34 @@ router.get('/', function(req, res) {
             req.session.access_token = saved_token;
         }
     }
+    //myFirebaseRef = new Firebase("https://FriendlyChat.firebaseio.com/");
+
+    var page = fs.readFileSync("web/view/principal.html", "utf8");
+    res.send(mustache.to_html(page, { show: false }));
+})
+
+
+router.get('/set', function(req, res) {
+    firebase.database().ref('/').set({
+        username: "test",
+        email: "test@mail.com"
+    });
+    firebase.database().ref('/').child('Tuyama COdao').set({
+        estudante: "test",
+        email: "test@mail.com"
+    });
+
+    var page = fs.readFileSync("web/view/principal.html", "utf8");
+    res.send(mustache.to_html(page, { show: false }));
+})
+
+router.get('/get', function(req, res) {
+    firebase.database().ref('/').on("value", function(snapshot) {
+        console.log("x");
+        console.log(snapshot.val()); // Alerts "something"
+        console.log("x");
+    });
+
 
     var page = fs.readFileSync("web/view/principal.html", "utf8");
     res.send(mustache.to_html(page, { show: false }));

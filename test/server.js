5e052e75ee06b815e00da9e4b06df385fe7f6a93
ftuyama <ftuyama@gmail.com>
2016-09-22 19:54:53 -0300

    Refatorando testes

--- test/server.js
+++ test/server.js
@@ -6,17 +6,17 @@ describe('server.js', function() {
     before(function() {
         this.timeout(5000);
     });
-    it('responds to /', function testSlash(done) {
+    it('200 responds to /', function testSlash(done) {
         request(server).get('/').expect(200, done);
     });
-    it('responds to /calendar', function testSlash(done) {
-        request(server).get('/calendar').expect(200, done);
+    it('302 redirects to /calendar', function testSlash(done) {
+        request(server).get('/calendar').expect(302, done);
     });
-    it('responds to /calendarAuth', function testSlash(done) {
-        request(server).get('/calendarAuth').expect(200, done);
+    it('302 redirects to /calendarAuth', function testSlash(done) {
+        request(server).get('/calendarAuth').expect(302, done);
     });
-    it('responds to /projectAuth', function testSlash(done) {
-        request(server).get('/projectAuth').expect(200, done);
+    it('302 redirects to /projectAuth', function testSlash(done) {
+        request(server).get('/projectAuth').expect(302, done);
     });
     it('404 everything else', function testPath(done) {
         request(server).get('/foo/bar').expect(404, done);

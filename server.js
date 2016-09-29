c69edb8259ecd992a75b5e676e1960e496918209
Felipe Tuyama <ftuyama@gmail.com>
2016-09-29 18:50:39 -0300

    Request Url relativa

--- server.js
+++ server.js
@@ -63,7 +63,6 @@ var port = process.env.PORT || 8080;
 var server = app.listen(port, function() {
     var host = server.address().address
     var port = server.address().port
-
     console.log("Crono app listening at port %s", port)
 })
 

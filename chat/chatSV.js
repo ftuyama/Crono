41ab9f04d7e25f348be764dc995164acb29b6317
joaovitormarques <joaovitormarques17@gmail.com>
2016-10-22 16:18:40 -0200

    chat user name

--- chat/chatSV.js
+++ chat/chatSV.js
@@ -12,26 +12,49 @@ var appl = require("../server");
 var server = appl.server;
 var redis = appl.redis;
 var io = require('socket.io')(server, { path: '/chatS' });
+var config = require('../config');
+var user;
 
 router.get('/', function(req, res) {
+    var accessToken = req.session.access_token;
+    if (accessToken == null) return res.redirect('/calendarAuth');
+    user = req.session.passport.user;
     res.sendFile(__dirname + '/chat.html');
 });
 
 io.on('connection', function(socket) {
     retrieveChatHistory();
-    console.log('a user connected');
-    redis.set("chat:connected:" + timeStamp(), 'user');
+
+    try {
+        console.log(user.displayName + ' connected');
+        redis.set("chat:connected: " + timeStamp(), user.displayName);
+        io.emit('user connected', user.displayName);
+    } catch (err) {
+        console.log('chat error ' + err);
+
+    }
     socket.on('disconnect', function() {
-        console.log('user disconnected');
-        redis.set("chat:disconnected:" + timeStamp(), 'user');
+        try {
+            console.log(user.displayName + 'disconnected');
+            redis.set("chat:disconnected: " + timeStamp(), user.displayName);
+            io.emit('user disconnected', user.displayName);
+        } catch (err) {
+            console.log('chat error ' + err);
+
+        }
     });
     socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
-        console.log('user sent' + msg);
-        redis.set("chat:talk:" + timeStamp(), JSON.stringify({
-            'user': 'user',
-            'message': msg
-        }));
+        try {
+            io.emit('chat message', msg);
+            console.log(user.displayName + ' sent: ' + msg);
+            redis.set("chat:talk:" + timeStamp(), JSON.stringify({
+                'user': user.displayName,
+                'message': msg
+            }));
+
+        } catch (err) {
+            console.log('error: ' + err);
+        }
     });
 });
 
